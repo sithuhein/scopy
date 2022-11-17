0c24c0706c3e8cf6861ef05fe877c0fc1100adf7
Bindea Cristian <cristian.bindea@analog.com>
2022-11-17 10:58:17 +0200

    ci:ubuntu: port ubuntu 18 and 20 to gnuradio 3.10
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

--- CI/appveyor/inside_flatpak_docker.sh
+++ CI/appveyor/inside_flatpak_docker.sh
@@ -12,7 +12,7 @@ REPO_LOCAL=/home/docker/scopy-flatpak
 cd "$REPO_LOCAL"
 # this ensures that latest master is pulled from origin while keeping file cache
 # the cache should be updated from time to time locally
-git fetch && git reset origin/master --hard
+git fetch && git reset origin/qt5-gr3.10 --hard
 
 # Run the preprocess step to generate org.adi.Scopy.json
 make preprocess
